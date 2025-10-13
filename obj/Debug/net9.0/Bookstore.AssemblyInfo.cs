47147c45f6895018fa17558af895b3cfb124f5f1
OXplakhoa <accbangbangchuot@gmail.com>
2025-10-13 16:34:55 +0700

    Add _ViewImport to enable tagHelper & Fix name of categories

--- obj/Debug/net9.0/Bookstore.AssemblyInfo.cs
+++ obj/Debug/net9.0/Bookstore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b70725f5ba96612087b24afe8b42246eecfe126")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f54da1088ac586aefff10ae28240780754bf93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
