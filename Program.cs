173842106a4798f6b3cd2f2aa8d9d59f22aa7f25
OXplakhoa <accbangbangchuot@gmail.com>
2025-10-19 16:02:09 +0700

    Scaffoding Identity Page

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
     options.Password.RequireDigit = true;
